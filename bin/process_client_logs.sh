7f34d00002f9080c44957ec5018da2adb7164a7d
Zeke Krug <krug0102@tardis.morris.umn.edu>
2021-09-09 11:28:37 -0500

    Updated process_client_logs.sh Added awk line to only keep the lines which pertain to Password failures.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,6 +6,6 @@
 cd "$1"
 
 #Combines all the text files in the specified directory.
-cat /var/log/*
+cat /var/log/* > logs.txt
 
-#awk 'match("$0", /([a-zA-Z0-9]:_[])/ Failed password for /([a-zA-Z0-9])/, groups) {print groups[1] groups[2]}'
+awk 'match("$0", /([a-zA-Z0-9:[])/: Failed password for /([a-zA-Z0-9\.])/, groups) {print groups[1] groups[2]}'<logs.txt>output.txt
