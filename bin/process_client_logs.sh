bfa8503e94037cd27a553489c6ddd4353a5e2d3a
Zeke Krug <krug0102@tardis.morris.umn.edu>
2021-09-09 11:13:40 -0500

    Updated process_client_logs.sh Since when process_client_logs.sh is called, the .tgz files in log_files will already have been unzipped, we edited our cat to not tar anything.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#Takes a directory as a command line argument and does all its work in that directory.
+#Takes a directory in log_files as a command line argument and does all its work in that directory.
 
 #Moves to the specified directory.
-cd ../"$1"
+cd "$1"
 
-cat *.tgz | tar -xzvf - -i
+#Combines all the text files in the specified directory.
+cat /var/log/*
 
 #awk 'match("$0", /([a-zA-Z0-9]:_[])/ Failed password for /([a-zA-Z0-9])/, groups) {print groups[1] groups[2]}'
