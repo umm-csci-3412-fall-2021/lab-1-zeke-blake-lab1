f749211f37b3fe8bcd3129a604d803da4b9aeb56
Zeke Krug <krug0102@tardis.morris.umn.edu>
2021-09-14 21:02:34 -0500

    Updated process_client_logs.sh Added line to pipe the concatenated log files into an awk statement to remove the unwanted information and only keen the desired 5 columns.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
 #cd into the directory and go into its subdirectories or exit if the cd failed
-cd "$1"/var/log || exit
+cd "$1" || exit
 
-#Concatenate all files in the directory
-#pipe cat into an awk command
-#then pipe that into a sed command
-#then print to failed_login_data.txt
-cat ./* | awk 'match("$0", /([a-zA-Z0-9]) Failed password ([a-zA-Z0-9.])/, groups) {print groups[1] groups[2]}' >  failed_login_data.txt
+cat var/log/* | awk 'match($0, /([a-zA-Z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4] "\n" }' > failed_login_data.txt
 
-
-#Move the failed_login_data.txt file back into directory stored in the directory variable
-mv failed_login_data.txt ../../
