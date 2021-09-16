87f54eec40e088fcefa070fb4d7921a1386c9b0c
Zeke Krug <krug0102@tardis.morris.umn.edu>
2021-09-16 10:16:18 -0500

    Updated process_client_logs Separated lines to make reading easier

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,5 +3,6 @@
 #cd into the directory and go into its subdirectories or exit if the cd failed
 cd "$1" || exit
 
-cat var/log/* | awk 'match($0, /([a-zA-Z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1]  groups[2]  groups[3] groups[4] "\n" }' > failed_login_data.txt
-
+cat var/log/* | \
+awk 'match($0, /([a-zA-Z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3] " " groups[4]}'\
+> failed_login_data.txt
