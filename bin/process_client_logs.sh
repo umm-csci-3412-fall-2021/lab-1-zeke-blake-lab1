62b20f13ad10047dcab97fe01ceeec0602e04148
Zeke Krug <krug0102@tardis.morris.umn.edu>
2021-09-21 11:32:50 -0500

    Updated process_client_logs.sh Fixed a problem where after the day, there would be two spaces if it was a single digit day.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -4,5 +4,7 @@
 cd "$1" || exit
 
 cat var/log/* | \
-awk 'match($0, /([a-zA-Z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3] " " groups[4]}'\
-> failed_login_data.txt
+
+awk 'match($0, /([a-zA-z]{3})  ?([0-9]{1,2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]  " " groups[5]}' > failed_login_data.txt
+
+#awk 'match($0, /([a-zA-Z]{3}) [[:space:]] ([0-9]{2}) [[:space:]] ([0-9]{2}) [[:space:]] ([a-zA-Z0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' < failed_login_data.txt > output.txt
